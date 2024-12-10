a7f53849d898210d4a5c318e0cd3297ce3c20db0
fc0597 <davide.marchignoli@allitude.it>
2024-12-10 15:12:09 +0100

    fix: bytestreammaterializer

--- src/haystack_rag/ingest.py
+++ src/haystack_rag/ingest.py
@@ -14,7 +14,6 @@ def load_library_urls():
     with open(os.path.join(BASE_DIR, 'resources/library_urls.txt')) as f:
         return [x.strip() for x in f.readlines()]
 
-
 CACHE_SUBDIR = '.cache/haystack'
 
 @component
@@ -23,10 +22,10 @@ class ByteStreamMaterializer():
     Saves the byte streams in input to filesystem returns the file paths
     """
     def __init__(self, out_dir: Optional[Path] = None):
-        if out_dir is None:
-            out_dir = os.path.join(os.getenv('HOME'), CACHE_SUBDIR)
-        out_dir.mkdir(parents=True)
-        self.out_dir = out_dir
+        self._out_dir = out_dir if out_dir is not None \
+          else Path(os.getenv('HOME')).joinpath(CACHE_SUBDIR)
+        if not self._out_dir.exists():
+          self._out_dir.mkdir(parents=True)
     
     @component.output_types(paths=List[Path])
     def run(self, bstreams: List[ByteStream]):
@@ -37,13 +36,12 @@ class ByteStreamMaterializer():
             filename = os.path.basename(parsed_url.path)
             if not filename:  # Handle cases where there's no filename in the URL
                 raise ValueError("Missing base name in {url}")
-            filepath = os.path.join(self.out_dir, filename)
+            filepath = self._out_dir.joinpath(filename)
             # Write the ByteStream content to the file
             bstream.to_file(filepath)
             paths.append(filepath)
-        return {paths: paths}
-    
-    
+        return {'paths': paths}
+
 # @component
 # class UnstructuredConverter(UnstructuredFileConverter):
 

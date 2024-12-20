2d056bf7495659baa84dbbc79da05c58243d52e6
fc0597 <davide.marchignoli@allitude.it>
2024-12-20 12:11:25 +0100

    dev: exported DocIdIndexer

--- src/haystack_rag/__init__.py
+++ src/haystack_rag/__init__.py
@@ -1,4 +1,5 @@
 from .gcs_document_store import GCSDocumentStore
 from .byte_stream_serializer import ByteStreamMaterializer
 from .doc_meta_fixer import DocMetaFixer
+from .doc_id_indexer import DocIdIndexer
 

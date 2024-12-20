b4beaac3ceeca80263fc10ccc82875f1cdf36623
fc0597 <davide.marchignoli@allitude.it>
2024-12-20 12:33:22 +0100

    dev: exported DocIdFilter

--- src/haystack_rag/__init__.py
+++ src/haystack_rag/__init__.py
@@ -2,4 +2,5 @@ from .gcs_document_store import GCSDocumentStore
 from .byte_stream_serializer import ByteStreamMaterializer
 from .doc_meta_fixer import DocMetaFixer
 from .doc_id_indexer import DocIdIndexer
+from .doc_id_filter import DocIdFilter
 
